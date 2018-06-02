0b3b5a1de899d56070027d7735a9ade20b503983
berajaalan <berajaalan@gmail.com>
2018-06-02 15:09:01 -0300

    Commit Inicial

---
+++ Bombeiros/src/bombeiros/Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bombeiros;
+
+/**
+ *
+ * @author beraj
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
