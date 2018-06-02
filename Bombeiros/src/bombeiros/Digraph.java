3064417726fe3a451b3796418741bfc9d24b4144
berajaalan <berajaalan@gmail.com>
2018-06-02 18:18:40 -0300

    Mudancas

---
+++ Bombeiros/src/bombeiros/Digraph.java
@@ -0,0 +1,94 @@
+package bombeiros;
+
+public class Digraph {
+
+    private int V; // número de vértices
+    private int A; // número de arcos
+    private No adj[]; // listas de adjcências
+
+    // construtor da clsse
+    // inicializa os atributos da classe e cria a 
+    // o vetor de listas deadjacências para V vértices
+    public Digraph(int V) {
+        this.V = V;
+        this.A = 0;
+        this.adj = new No[V];
+    }
+
+    /* 
+           Método insere um arco v-w no digrafo. O método supõe 
+           que v e w são distintos, positivos e menores que V. 
+           Se o digrafo já tem arco v-w, o método não faz nada.
+           Insere aresta na lista de adjacencias no final
+     */
+    public void insereA(int v, int w) {
+        No aux = this.adj[v];
+        No ant = null;
+        while (aux != null) {
+            //Se o digrafo já tem arco v-w, o método não faz nada
+            if (aux.getElem() == w) {
+                return; // ejeta
+            }
+            ant = aux;
+            aux = aux.getProx();
+        }
+        No novo = new No(w, null);
+        this.A++;// incrementa o numero de arcos
+        // testa se a lista esta vazia
+        if (ant == null) // se lista vazia
+        // joga no inicio da lista
+        {
+            this.adj[v] = novo;
+        } else // insere no final da lista
+        {
+            ant.setProx(novo);
+        }
+    }
+
+    /*
+           Método remove do digrafo um arco aresta v-w. 
+           O método supõe que v e w são distintos, positivos e 
+           menores que V. Se não existe um arco v-w, método não 
+           faz nada.
+     */
+    public void removeA(int v, int w) {
+
+    }
+
+    /*
+           Para cada vértice v do grafo, este método imprime, em 
+           uma linha, todos os vértices adjacentes a v. 
+     */
+    public void mostra() {
+
+        for (int i = 0; i < this.V; i++) {
+            System.out.print(i + ": ");
+            No aux = this.adj[i];
+            while (aux != null) {
+                System.out.print(aux.getElem() + ", ");
+                aux = aux.getProx();
+            }
+            System.out.println();
+        }
+    }
+
+    /*
+        método indeg que calcula o grau de entrada um 
+        vértice v de um digrafo.
+        Grau de Entrada: número de arcos com ponta final em v
+     */
+    public int indeg(int v) {
+
+        return 0;
+    }
+
+    /*
+        método outdeg que calcula o grau de saida de um 
+        vértice v em um digrafo.
+        Grau de saida: número de arcos com ponta inicial em v
+     */
+    public int outdeg(int v) {
+
+        return 0;
+    }
+}
--- Bombeiros/src/bombeiros/Mapa.java
+++ Bombeiros/src/bombeiros/Mapa.java
@@ -1,11 +1,81 @@
 package bombeiros;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 class Mapa {
     
     String path = System.getProperty("user.dir") + "\\mapa.txt";
-    No [] mapa;
+    Digraph mapa;
     
-    Mapa (){
+    Mapa () throws FileNotFoundException, IOException{
+        
+        FileReader fr = new FileReader(path);
+        BufferedReader br = new BufferedReader(fr);
+        
+        mapa = new Digraph(Integer.parseInt(br.readLine()));
+        int ln = 1;
+        
+        while(true){
+            
+            // Definir variaveis
+            String l[] = br.readLine().split(" ");
+            int x = Integer.parseInt(l[0]),
+                y = Integer.parseInt(l[1]);
+            boolean ciclo = false, ins = false;
+            
+            // Condição de parada
+            if (x == 0 && y == 0) {
+                break;
+            }
+            
+            // Mensagens de erro igualdade
+            if (x == y) {
+                System.out.println("Linha " + ln +", Par " + x + "," + y + ": x deve ser diferente do y.");
+            }
+            
+            for (int i = 0; i < mapa.length; i++) {
+                
+                // Mensagem de erro forma ciclo
+                if (i == y-1) {
+                    while(mapa[i].getProx() != null){
+                        if (mapa[i].getElem() == x) {
+                            System.out.println("Linha " + ln +", Par " + x + "," + y + ": forma ciclo.");
+                            ciclo = true;
+                        }
+                    }
+                }
+                
+                // Mensagem de erro já inserido
+                if (i == x-1) {
+                    while(mapa[i].getProx() != null){
+                        if (mapa[i].getElem() == y) {
+                            System.out.println("Linha " + ln +", Par " + x + "," + y + ": já inserido.");
+                            ins = true;
+                        }
+                    }
+                } 
+            }
+            
+            // Definir conexoes
+            if (x != y && !ciclo && !ins) {
+                if (ln == 1) {
+                    mapa[x-1] = new No(y,null);
+                }else{
+                    while (true){
+                        No 
+                        if () {
+                            
+                        }
+                    }
+                }
+            }
+            
+            // Define linha atual do arquivo
+            ln ++;
+        }
         
     }
     
